7197ea6e61f09ec329c72a280a66b62b14e45f65
Arkadiusz <108217975+ArkadiuszBorcz94@users.noreply.github.com>
2023-06-27 17:25:56 +0200

    NumberGuesser
    Guess Number form 1 to 10

---
+++ nauka/nauka/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+
+
+
+namespace NumberGuesser
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetAppInfo();
+            string userName=GetUserName();
+
+            GreetUser(userName);
+
+            Random random=new Random();
+            int correctNumber = random.Next(1,11);
+            bool correctAnswer = false;
+
+            Console.WriteLine("Zgadnij wylosowaną liczbe od 1 do 10");
+
+            while (correctAnswer == false) 
+            {
+                string input = Console.ReadLine();
+
+                int guess;
+
+              bool isNumber=  int.TryParse(input, out guess);
+
+
+                if (!isNumber)
+                {
+                    PrintColorMessage(ConsoleColor.Yellow, "Prosze wprowadzić lliczbe.");
+                    continue;
+
+
+                }
+
+
+                if (guess < 1 || guess > 10) 
+                {
+
+                    PrintColorMessage(ConsoleColor.Yellow, "Prosze wprowadzić liczbe od 1 do 10");
+                    continue;
+                }
+
+
+                if (guess < correctNumber) 
+                {
+                    PrintColorMessage(ConsoleColor.Red, "Błędna odpowiedź wylosowana liczba jest WIĘKSZA");
+                    continue;
+                
+                }
+               else if (guess > correctNumber)
+                {
+                    PrintColorMessage(ConsoleColor.Red, "Błędna odpowiedź wylosowana liczba jest MNIEJSZA");
+                    continue;
+
+                }
+
+                else
+                {
+                    PrintColorMessage(ConsoleColor.Green, "Gratulacje ODGADŁEŚ LICZBE");
+                    continue;
+
+                }
+
+
+
+            }
+
+
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Zgadywanie liczby";
+            int appVersion = 1;
+            string appAuthor = "Arkadiusz Borcz";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ResetColor();
+
+            string info = $"{appName} Version: 0.0.{appVersion}   Autor: {appAuthor}  ";
+
+            PrintColorMessage(ConsoleColor.Magenta, info);
+
+        }
+
+        static string GetUserName() 
+        {
+            Console.WriteLine("Jak masz na imie?");
+            string inputUserName = Console.ReadLine();
+
+            return inputUserName;
+        
+        }
+
+
+        static void GreetUser(string userName)
+
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            string greet= $"Powodzenia {userName}, odgadnij liczbę....  ";
+
+            Console.ResetColor();
+
+           
+            PrintColorMessage(ConsoleColor.Blue, greet);
+
+
+        }
+
+
+
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+
+        
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
